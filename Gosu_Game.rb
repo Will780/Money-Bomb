92bf780bac8097ceef5a04f0319e161578bc43bd
Will North <will.north@comcast.net>
2019-10-11 10:09:14 -0600

    Complete step 2.2 & download images

--- Gosu_Game.rb
+++ Gosu_Game.rb
@@ -1,23 +1,5 @@
 require 'gosu'
 
-class Tutorial < Gosu::Window
-    def initialize
-        super 640, 480
-        self.caption = "Tutorial Game"
-
-        @background_image = Gosu::Image.new("media/space.png", :tileable => true)
-    end
-
-    def update
-        #...
-    end
-
-    def draw
-        @background_image.draw(0, 0, 0)
-    end
-end
-Tutorial.new.show
-
 class Player
     def initialize
         @image = Gosu::Image.new("media/starfighter.bmp")
@@ -56,3 +38,44 @@ class Player
         @image.draw_rot(@x, @y, 1, @angle)
     end
 end
+
+class Tutorial < Gosu::Window
+    def initialize
+        super 640, 480
+        self.caption = "Tutorial Game"
+
+        @background_image = Gosu::Image.new("media/space.png", :tileable => true)
+
+        @player = Player.new
+        @player.warp(320, 240)
+    end
+
+    def update
+        if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+            @player.turn_left
+        end
+        if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+            @player.turn_right
+        end
+        if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+            @player.accelerate
+        end
+        @player.move
+    end
+
+    def draw
+        @player.draw
+        @background_image.draw(0, 0, 0)
+    end
+
+    def button_down(id)
+        if id == Gosu::KB_ESCAPE
+            close
+        else
+            super
+        end
+    end
+end
+
+Tutorial.new.show
+
